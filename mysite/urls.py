610537a413d18f184f94d32e3715a87dd61b2742
MohammadAsgaryan <mohamadasgaryanshad@gmail.com>
2025-12-29 21:05:28 +0330

    created app

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import http_test,jeson_test
+from django.urls import path,include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('http-test' ,http_test),
-    path('jeson-test' , jeson_test),
+    path('',include('website.urls') )
 ]
--- mysite/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-
-def http_test(request):
-    return HttpResponse("<h1>this is a test<h1>")
-
-def jeson_test(request):
-    return JsonResponse({'name':'mmd'})
