d64a65ec466e62473de83c8a6e5f83101efc41ba
MohammadAsgaryan <mohamadasgaryanshad@gmail.com>
2025-12-31 00:11:03 +0330

    add url

--- website/views.py
+++ website/views.py
@@ -8,5 +8,10 @@ def about_view(request):
     return render(request ,'website/about.html')
 
 def contact_view(request):
-    return render(request ,'website/contact.html')
+    return render(request, 'website/contact.html')
 
+def travel_view(request):
+    return render(request, 'website/travel.html')
+
+def photography_view(request):
+    return render(request, 'website/photography.html')
