ebad6272d0397c1beeefab40705808fb43bc527a
MohammadAsgaryan <mohamadasgaryanshad@gmail.com>
2026-01-04 15:00:02 +0330

    create models

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,15 @@ from django.db import models
 
 # Create your models here.
 class post(models.Model):
+    # image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=200)
     content = models.TextField()
+    #category = models.CharField(max_length=100)
+    #tags = models.CharField(max_length=200)
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    #author = models.CharField(max_length=100)
+    
